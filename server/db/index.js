5c0085164bb8eb07e2d8033f78adc662588d9ca3
Puneet Khoiya <puneetkhoiyabnl8@gmail.com>
2025-08-09 04:07:04 +0530

    readme fixed

--- server/db/index.js
+++ server/db/index.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || '127.0.0.1',
+  host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || 'Puneet7986973223@',
-  database: process.env.DB_NAME || 'multiplex_db',
+  password: process.env.DB_PASS || '',
+  database: process.env.DB_NAME || 'test',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
